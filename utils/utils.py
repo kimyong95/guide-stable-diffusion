7ecdf154cc9ec419cadf07648d1567936d1abb71
Tan Kim Yong <kimyong95@gmail.com>
2024-08-16 00:27:09 +0800

    fix bug

--- utils/utils.py
+++ utils/utils.py
@@ -37,6 +37,7 @@ def find_closest_factors(n):
     for i in range(int(math.sqrt(n)), n):
         if n % i == 0:
             return i
+    return 0
 
 def disable_train(model: torch.nn.Module):
     model.eval()
