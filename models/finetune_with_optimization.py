8670ca072a98310f8d256352516c31e38ce8c0db
Tan Kim Yong <kimyong95@gmail.com>
2024-11-08 15:56:52 +0800

    update

--- models/finetune_with_optimization.py
+++ models/finetune_with_optimization.py
@@ -55,7 +55,7 @@ class FinetuneDiffusionWithOptimization(DiffusionBase):
 
         batch_noise_original_norm = batch_noise_original.norm(dim=-1)
         batch_noise_norm = batch_noise.norm(dim=-1)
-        batch_noise_projected = batch_noise * ( batch_noise_original_norm[:,:,None] / batch_noise_norm[:,:,None] )
+        batch_noise_projected = batch_noise / batch_noise_norm[:,:,None] * batch_noise_original_norm[:,:,None]
 
         batch_noise = self._x_unflatten(batch_noise)
         batch_noise_projected = self._x_unflatten(batch_noise_projected)
